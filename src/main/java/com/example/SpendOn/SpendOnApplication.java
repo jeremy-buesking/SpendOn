a4574a860c5949b58e26503b5e0eabfabce91959
jeremy-buesking <jeremy.buesking@gmail.com>
2023-09-02 21:52:40 -0500

    First Commit

---
+++ src/main/java/com/example/SpendOn/SpendOnApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpendOn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendOnApplication.class, args);
+		System.out.println("Hello Ya'll");
+	}
+}
---
+++ src/test/java/com/example/SpendOn/SpendOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpendOn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
